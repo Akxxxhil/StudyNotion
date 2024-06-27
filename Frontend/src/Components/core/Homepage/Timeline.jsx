f1159ca868a6ae11d82d498c1e049b7a77a9022a
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-06-27 21:25:44 +0530

    css updated

--- Frontend/src/Components/core/Homepage/Timeline.jsx
+++ Frontend/src/Components/core/Homepage/Timeline.jsx
@@ -35,47 +35,52 @@ const timeline = [
 function Timeline() {
     return (
         <div>
-            <div className='bg-white text-black '>
-                <div className='flex flex-col lg:flex-row gap-20 mb-20 items-center'>
-                    <div className='lg:w-[45%] flex flex-col gap-14 lg:gap-3'>
-                        {
-                            timeline.map((item) => {
-                                return (
-                                    <div key={item.id} className='flex flex-col lg:gap-3'>
-                                        <div className='flex gap-6'>
-                                            <div className='w-[52px] h-[52px] bg-white rounded-full flex justify-center items-center shadow-[#00000012] shadow-[0_0_62px_0]'>
-                                                <img src={item.Logo} alt="" />
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <p className='font-semibold text-[18px]'>{item.Heading}</p>
-                                            <p className='text-base'>{item.Description}</p>
-                                        </div>
+        <div className='flex flex-col lg:flex-row gap-20 mb-20 items-center'>
+            <div className='lg:w-[45%] flex flex-col gap-14 lg:gap-3'>
+                {
+                    timeline.map( (element, index) => {
+                        return (
+                            <div className='flex flex-col lg:gap-3' key={index}> 
+                                <div className='flex gap-6'>
+                                    <div className='w-[52px] h-[52px] bg-white rounded-full flex justify-center items-center shadow-[#00000012] shadow-[0_0_62px_0]'>
+                                        <img src={element.Logo} alt={`${element.Logo}.icon`} />
                                     </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='relative w-fit h-fit shadow-blue-200 shadow-[0px_0px_30px_0px]'>
-                        <div className='absolute lg:left-[50%] lg:bottom-0 lg:translate-x-[-50%] lg:translate-y-[50%] bg-caribbeangreen-700 flex lg:flex-row flex-col text-white uppercase py-5 gap-4 lg:gap-0 lg:py-10 '>
-                            <div className='flex gap-5 items-center lg:border-r border-caribbeangreen-300 px-7 lg:px-14'>
-                                <p className='text-3xl font-bold w-[75px]'>10</p>
-                                <p className='text-caribbeangreen-300 text-sm w-[75px]'>years experience</p>
+                                    <div>
+                                        <h2 className='font-semibold text-[18px]'>
+                                            {element.Heading}
+                                        </h2>
+                                        <p className='text-base'>
+                                            {element.Description}
+                                        </p>
+                                    </div>
+                                </div>
+                                {index < 3 && (
+                                    <div className='hidden lg:block h-14 border-dotted border-r border-richblack-100 bg-richblack-400/0 w-[26px]'></div>
+                                )}
                             </div>
-                            <div className='flex gap-5 items-center lg:px-14 px-7'>
-                                <p className='text-3xl font-bold w-[75px]'>250</p>
-                                <p className='text-caribbeangreen-300 text-sm w-[75px]'>Types of courses</p>
-                            </div>
-                        </div>
-                        <img src={timelineimage}
-                            alt='timelineImage'
-                            className='shadow-white shadow-[20px_20px_0px_0px] object-cover h-[400px] lg:h-fit'
-                        />
-                    </div>
+                        )
+                    } )
+                }   
+            </div>
 
+            <div className='relative w-fit h-fit shadow-blue-200 shadow-[0px_0px_30px_0px]'>
+                <div className='absolute lg:left-[50%] lg:bottom-0 lg:translate-x-[-50%] lg:translate-y-[50%] bg-caribbeangreen-700 flex lg:flex-row flex-col text-white uppercase py-5 gap-4 lg:gap-0 lg:py-10 '>
+                    <div className='flex gap-5 items-center lg:border-r border-caribbeangreen-300 px-7 lg:px-14'>
+                        <p className='text-3xl font-bold w-[75px]'>10</p>
+                        <p className='text-caribbeangreen-300 text-sm w-[75px]'>years experience</p>
+                    </div>
+                    <div className='flex gap-5 items-center lg:px-14 px-7'>
+                        <p className='text-3xl font-bold w-[75px]'>250</p>
+                        <p className='text-caribbeangreen-300 text-sm w-[75px]'>Types of courses</p>
+                    </div>
                 </div>
+                <img src={timelineimage} 
+                    alt='timelineImage'
+                    className='shadow-white shadow-[20px_20px_0px_0px] object-cover h-[400px] lg:h-fit'
+                />
             </div>
         </div>
+    </div>
     )
 }
 

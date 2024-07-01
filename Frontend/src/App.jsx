6cee2e5d9eb0ec247380b1fbd3152d9d29487eaf
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-01 14:56:18 +0530

    message

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Signup from './Components/core/auth/Signup'
+import Login from './Components/core/auth/Login'
 
 function App() {
   return (
     <div className='w-screen min-h-screen bg-[#000814] flex flex-col font-inter'>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   )
---
+++ Frontend/src/Components/core/auth/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='text-white flex flex-col justify-center items-center'>
+        <h1>Signup is on working keep calm</h1>
+   <h2>Backend part done ... you can check</h2>
+   <a className='text-blue-400 underline' href="https://github.com/Akxxxhil/StudyNotion/tree/main/Backend">Backend Repo</a>
+   
+    </div>
+  )
+}
+
+export default Signup
